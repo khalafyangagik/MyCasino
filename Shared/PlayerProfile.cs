3fc063d71e4a88f922ca503cd992b1d0846df0d6
Gagik <khalafyangagik69@gmail.com>
2025-09-24 21:26:11 +0400

    Changes

---
+++ Shared/PlayerProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Casino.Core.Models;
+using Shared.DTOs;
+
+namespace Shared
+{
+    public class PlayerProfile : Profile
+    {
+        public PlayerProfile()
+        {
+            CreateMap<PlayerDto,Player>().ReverseMap();
+        }
+
+    }
+}
